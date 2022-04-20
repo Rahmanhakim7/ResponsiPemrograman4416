c8ecaedd0a858a1d4236430282787ff879a776f9
rahmanhakim7 <hakimrahman@students.amikom.ac.id>
2022-04-20 09:27:06 +0700

    Add project files.

---
+++ ResponsiPemrograman4416/Karyawan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4416
+{
+    internal class Karyawan
+    {
+        public string NIK { get; set; }
+        public string Nama { get; set; }
+        public float Gajian { get; set; }
+        public Karyawan(string nik, string nama, float gaji)
+        {
+            NIK = nik;
+            Nama = nama;
+            if (gaji < 0)
+            {
+                gaji = 0;
+            }
+            Gajian = gaji;
+        }
+
+        public void NaikkanGaji(float persentase)
+        {
+            Gajian = persentase / 100 * Gajian + Gajian;
+        }
+    }
+}
---
+++ ResponsiPemrograman4416/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4416
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Karyawan org1 = new Karyawan("190302123", "Rahman", 3000000);
+            Karyawan org2 = new Karyawan("190302124", "Hakim", 2000000);
+            Console.WriteLine("Program Gaji Bulanan Pt Artha Mulia Abadi");
+            Console.WriteLine();
+            Console.WriteLine("No      Nik/Nama            Gaji Bulanan");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("1       {0}/{1}         {2}", org1.NIK, org1.Nama, org1.Gajian);
+            Console.WriteLine("2       {0}/{1}          {2}", org2.NIK, org2.Nama, org2.Gajian);
+
+            Console.WriteLine();
+            Console.WriteLine("Berkah Ramadhan gaji naik 10%!");
+            Console.WriteLine();
+
+            org1.NaikkanGaji(10);
+            org2.NaikkanGaji(10);
+
+            Console.WriteLine("No      Nik/Nama            Gaji Bulanan");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("1       {0}/{1}         {2}", org1.NIK, org1.Nama, org1.Gajian);
+            Console.WriteLine("2       {0}/{1}          {2}", org2.NIK, org2.Nama, org2.Gajian);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
